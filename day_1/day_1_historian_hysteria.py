5846f36257a7e1c1fa6db345908c3b6768fb1a05
Michael McKay <mmckay99@users.noreply.github.com>
2024-12-03 17:49:34 +0000

    tidying up d1p1

--- day_1/day_1_historian_hysteria.py
+++ day_1/day_1_historian_hysteria.py
@@ -4,7 +4,6 @@
 # two puzzles each day, one star for each completed, the second is unlocked after the first is completed
 
 # instructions are to sort each list, and add up the pairwise differences
-import csv
 import operator
 
 test_lists = [
@@ -13,9 +12,9 @@ test_lists = [
 ]
 
 def sum_of_pairwise_absolute_differences(lists):
-     return sum([
+     return sum(
         abs(operator.sub(*pair_of_numbers)) for pair_of_numbers in zip(*map(sorted, lists))
-    ])
+    )
     
 if __name__ == "__main__":
     with open('day_1_raw_data.csv') as raw_csv_data:
