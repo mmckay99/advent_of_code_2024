8b3385c1b0f0ce91474f3503c962bde3272e62aa
Michael McKay <mmckay99@users.noreply.github.com>
2024-12-05 12:07:40 +0000

    added day 2

--- day_1/day_1_historian_hysteria.py
+++ day_1/day_1_historian_hysteria.py
@@ -5,6 +5,7 @@
 
 # instructions are to sort each list, and add up the pairwise differences
 import operator
+import collections
 
 test_lists = [
     [3, 4, 2, 1, 3, 3],
@@ -16,12 +17,29 @@ def sum_of_pairwise_absolute_differences(lists):
         abs(operator.sub(*pair_of_numbers)) for pair_of_numbers in zip(*map(sorted, lists))
     )
     
-if __name__ == "__main__":
-    with open('day_1_raw_data.csv') as raw_csv_data:
-        number_pairs_by_line = [list(map(int, line.split())) for line in raw_csv_data]
-        actual_lists = [
-            [number_pair[0] for number_pair in number_pairs_by_line],
-            [number_pair[1] for number_pair in number_pairs_by_line],
-        ]
+def calculate_similarity_score(lists):
+    # setting up the hash maps takes O(n) time, note that 
+    # in theory 
+    left_list_occurrences = collections.Counter(lists[0])
+    right_list_occurrences = collections.Counter(lists[1])
 
-        print(sum_of_pairwise_absolute_differences(actual_lists))
+    total_similarity = 0
+    for (location, location_occurrences_left) in left_list_occurrences.items():
+        total_similarity += location * location_occurrences_left * right_list_occurrences[location]
+
+    return total_similarity
+
+
+def read_csv_raw_data(filename):
+    raw_csv_data = open('day_1_raw_data.csv')
+    
+    number_pairs_by_line = [list(map(int, line.split())) for line in raw_csv_data]
+    return [
+        [number_pair[0] for number_pair in number_pairs_by_line],
+        [number_pair[1] for number_pair in number_pairs_by_line],
+    ]   
+
+if __name__ == "__main__": 
+    actual_lists = read_csv_raw_data("day_1_historian_hysteria.csv")
+    # print(sum_of_pairwise_absolute_differences(actual_lists))
+    print(calculate_similarity_score(actual_lists))
---
+++ day_2/day_2_red_nosed_reports.py
@@ -0,0 +1,55 @@
+test_report_data = [
+    [7, 6, 4, 2, 1],
+    [1, 2, 7, 8, 9],
+    [9, 7, 6, 2, 1],
+    [1, 3, 2, 4, 5],
+    [8, 6, 4, 4, 1],
+    [1, 3, 6, 7, 9]
+]
+
+def read_csv_raw_data(filename):
+    raw_csv_data = open('day_2_raw_data.txt')
+    return [list(map(int, line.split())) for line in raw_csv_data]
+
+def report_is_safe(report):
+    """
+    >>> report_is_safe([7, 6, 4, 2, 1])
+    True
+
+    >>> report_is_safe([1, 2, 7, 8, 9])
+    False
+    """
+    if len(report) < 2:
+        return True
+    
+    first_pair_is_increasing = (report[0] < report[1])
+    
+    for (level, next_level) in zip(report, report[1:]):
+        this_pair_is_increasing = (level < next_level)
+        if (first_pair_is_increasing != this_pair_is_increasing) or not (1 <= abs(next_level - level) <= 3):
+            return False
+        
+    return True
+
+def report_is_safe_dampener_version(report):
+    """
+    >>> report_is_safe_dampener_version([1, 3, 2, 4, 5])
+    True
+
+    >>> report_is_safe_dampener_version([9, 7, 6, 2, 1])
+    False
+    """
+    for index_of_level_to_remove in range(0, len(report)):
+        report_with_this_level_removed = report[0:index_of_level_to_remove] + report[index_of_level_to_remove + 1:]
+
+        if report_is_safe(report_with_this_level_removed):
+            return True
+        
+    return False
+
+if __name__ == "__main__":
+    actual_report_data = read_csv_raw_data('day_2_raw_data.txt')
+
+    print(
+        list(map(report_is_safe_dampener_version, actual_report_data)).count(True)
+    )
